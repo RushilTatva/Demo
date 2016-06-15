3288775ecdd3b1a27eca80a05a58a234e4f106df
unknown <PCI168@pci168.server7.com>
2016-06-15 18:53:15 +0530

    Upload Source Code first time.

---
+++ LappiaSPWeb.Root/LappiaSPWeb.Common/Comman.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LappiaSPWeb.Common
+{
+    public class Comman
+    {
+        public const string ResourceFilename = "LappiaRoot";
+    }
+}
